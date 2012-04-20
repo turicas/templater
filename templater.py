590d07ce4a6df0c66627b2f75912abde68b1c646
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-04-20 18:07:04 -0300

    ENH: _create_template now accepts
    - Modified _create_template signature and now accepting tolerance
    - Modified tests to _create_template and added tolerance tests
    - Removed iterative version of _create_template

--- templater.py
+++ templater.py
@@ -8,6 +8,7 @@ from _templater import longest_match as lcs
 class Templater(object):
     def __init__(self, template=None, marker='|||'):
         self._template = template
+        self._tolerance = 0
         if type(template) in (str, unicode):
             tokens = template.split(marker)
             self._template = list(sum(zip([None] * len(tokens), tokens), ())) \
@@ -18,7 +19,8 @@ class Templater(object):
             text = new_text
         else:
             text = '\0\0\0'.join([x for x in self._template if x is not None])
-        self._template = _create_template(new_text, text)
+        self._template = _create_template(new_text, text, (0, len(new_text)),
+                                          (0, len(text)), self._tolerance)
 
     def parse(self, text):
         return _parser(self._template, text)
@@ -81,41 +83,19 @@ def _parser(template, text):
             text_index += element_length
     return result
 
-def template_iterative(s1, s2):
-    result = []
-    insert = 0
-    start_1, end_1 = 0, len(s1)
-    start_2, end_2 = 0, len(s2)
-    while True:
-        size, s1start, s2start = lcs(s1[start_1:end_1], s2[start_2:end_2])
-        string_to_add = s1[start_1 + s1start:start_1 + s1start + size]
-        if string_to_add:
-            add_to_result = [
-                    ((start_1, start_1 + s1start), (start_2, start_2 + s2start)),
-                    string_to_add,
-                    ((start_1 + s1start + size, end_1), (start_2 + s2start + size, end_2))]
-        else:
-            add_to_result = [None]
-        result[insert:insert + 1] = add_to_result
-        continue_processing = False
-        for index, element in enumerate(result):
-            if type(element) == tuple:
-                next_to_process = element
-                start_1, end_1 = element[0]
-                start_2, end_2 = element[1]
-                insert = index
-                continue_processing = True
-                break
-        if not continue_processing:
-            break
-    return result
-
-def template_recursive(s1, s2):
-    size, s1start, s2start = lcs(s1, s2)
-    if size == 0:
+def _create_template(str_1, str_2, (start_1, end_1), (start_2, end_2),
+                     tolerance=0):
+    lcs_size, lcs_1_start, lcs_2_start = lcs(str_1[start_1:end_1],
+                                         str_2[start_2:end_2])
+    if lcs_size <= tolerance:
         return [None]
-    return template_recursive(s1[:s1start], s2[:s2start]) + \
-            [s1[s1start:s1start + size]] + \
-            template_recursive(s1[s1start + size:], s2[s2start + size:])
-
-_create_template = template_recursive
+    else:
+        return _create_template(str_1, str_2,
+                                (start_1, start_1 + lcs_1_start),
+                                (start_2, start_2 + lcs_2_start),
+                                tolerance) + \
+               [str_1[start_1 + lcs_1_start:start_1 + lcs_1_start + lcs_size]] + \
+               _create_template(str_1, str_2,
+                                (start_1 + lcs_1_start + lcs_size, end_1),
+                                (start_2 + lcs_2_start + lcs_size, end_2),
+                                tolerance)
--- tests/test_template.py
+++ tests/test_template.py
@@ -5,66 +5,105 @@ from templater import _create_template
 
 
 def test_template_creation_0():
-    result = _create_template('1', '2')
+    str_1 = '1'
+    str_2 = '2'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None]
     assert result == expected
 
 def test_template_creation():
-    result = _create_template('a1', 'a2')
+    str_1 = 'a1'
+    str_2 = 'a2'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, 'a', None]
     assert result == expected
 
 def test_template_creation_2():
-    result = _create_template('ab1', 'ab2')
+    str_1 = 'ab1'
+    str_2 = 'ab2'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, 'ab', None]
     assert result == expected
 
 def test_template_creation_3():
-    result = _create_template('1a', '2a')
+    str_1 = '1a'
+    str_2 = '2a'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, 'a', None]
     assert result == expected
 
 def test_template_creation_4():
-    result = _create_template('abcdef', 'qbedff')
+    str_1 = 'abcdef'
+    str_2 = 'qbedff'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, 'b', None, 'd', None, 'f', None]
     assert result == expected
 
 def test_template_creation_5():
-    result = _create_template('abcccc', 'abe')
+    str_1 = 'abcccc'
+    str_2 = 'abe'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, 'ab', None]
     assert result == expected
 
 def test_template_creation_6():
-    result = _create_template('ccccab', 'eab')
+    str_1 = 'ccccab'
+    str_2 = 'eab'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, 'ab', None]
     assert result == expected
 
 def test_equal_strings_should_return_one_element():
-    result = _create_template('<b> asd </b>', '<b> asd </b>')
+    str_1 = '<b> asd </b>'
+    str_2 = '<b> asd </b>'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, '<b> asd </b>', None]
     assert result == expected
 
 def test_different_strings_with_same_size():
-    result = _create_template('<b> asd </b>', '<b> qwe </b>')
+    str_1 = '<b> asd </b>'
+    str_2 = '<b> qwe </b>'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, '<b> ', None, ' </b>', None]
     assert result == expected
 
 def test_different_strings_with_different_size():
-    result = _create_template('<b> asd </b>', '<b> qwe123 </b>')
+    str_1 = '<b> asd </b>'
+    str_2 = '<b> qwe123 </b>'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, '<b> ', None, ' </b>', None]
     assert result == expected
 
 def test_different_strings_with_one_of_size_zero():
-    result = _create_template('<b> asd </b>', '<b>  </b>')
+    str_1 = '<b> asd </b>'
+    str_2 = '<b>  </b>'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, '<b> ', None, ' </b>', None]
     assert result == expected
 
 def test_more_than_one_variable_with_same_size():
-    result = _create_template('<b> asd </b><u> 123 </u>', '<b> qwe </b><u> 456 </u>')
+    str_1 = '<b> asd </b><u> 123 </u>'
+    str_2 = '<b> qwe </b><u> 456 </u>'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, '<b> ', None, ' </b><u> ', None, ' </u>', None]
     assert result == expected
 
 def test_more_than_one_variable_with_different_sizes():
-    result = _create_template('<b> asdfgh </b><u> 123 </u>', '<b> qwe </b><u> 456qwe </u>')
+    str_1 = '<b> asdfgh </b><u> 123 </u>'
+    str_2 = '<b> qwe </b><u> 456qwe </u>'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)))
     expected = [None, '<b> ', None, ' </b><u> ', None, ' </u>', None]
+    assert result == expected
+
+def test_tolerance():
+    str_1 = 'my favorite color is blue'
+    str_2 = 'my favorite color is violet'
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)),
+                              tolerance=0)
+    expected = [None, 'my favorite color is ', None, 'l', None, 'e', None]
+    assert result == expected
+
+    result = _create_template(str_1, str_2, (0, len(str_1)), (0, len(str_2)),
+                              tolerance=1)
+    expected = [None, 'my favorite color is ', None]
     assert result == expected
