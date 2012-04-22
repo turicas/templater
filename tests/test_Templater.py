db1d60543aa6a42972a7259c696b570b36abe391
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-04-22 20:00:31 -0300

    ENH: tests refactoring

--- tests/test_Templater.py
+++ tests/test_Templater.py
@@ -5,7 +5,20 @@ from os import unlink
 from re import compile as re_compile
 from templater import Templater, MARKER, NAMED_MARKER
 
+
 regexp_marker = re_compile(r'{{([a-zA-Z0-9_-]*)}}')
+
+def read_file_and_delete(filename):
+    fp = open(filename)
+    contents = fp.read()
+    fp.close()
+    unlink(filename)
+    return contents
+
+def write_file(filename, contents):
+    fp = open(filename, 'w')
+    fp.write(contents)
+    fp.close()
 
 def test_new_learn_text_trying_to_delete_some_variable():
     template = Templater()
@@ -82,10 +95,7 @@ def test_Templater_save_should_save_template_as_a_raw_file_with_markers():
     processed_template = [None, '<b>', None, '</b><u>', None, '</u>', None]
     t = Templater(template=processed_template)
     t.save('test.html', marker='|||')
-    fp = open('test.html')
-    result = fp.read()
-    fp.close()
-    unlink('test.html')
+    result = read_file_and_delete('test.html')
     expected = '|||<b>|||</b><u>|||</u>|||'
     assert expected == result
 
@@ -101,9 +111,7 @@ def test_Templater_should_accept_named_markers_in_init():
     assert expected_2 == result_2
 
 def test_Templater_open_should_load_template_from_a_raw_file_with_markers():
-    fp = open('test.html', 'w')
-    fp.write('|||<b>|||</b><u>|||</u>|||')
-    fp.close()
+    write_file('test.html', '|||<b>|||</b><u>|||</u>|||')
     t = Templater.open('test.html', marker='|||')
     unlink('test.html')
     result = t._template
@@ -111,9 +119,8 @@ def test_Templater_open_should_load_template_from_a_raw_file_with_markers():
     assert expected == result
 
 def test_named_markers_should_work():
-    fp = open('test.html', 'w')
-    fp.write('|||[first]<b>|||[second]</b><u>|||[third]</u>|||[fourth]')
-    fp.close()
+    write_file('test.html',
+               '|||[first]<b>|||[second]</b><u>|||[third]</u>|||[fourth]')
     t = Templater.open('test.html', marker=re_compile(r'\|\|\|\[([^\]]+)\]'))
     unlink('test.html')
     result_1 = t._template
@@ -126,9 +133,7 @@ def test_named_markers_should_work():
     assert expected_2 == result_2
 
 def test_should_not_have_named_marks_without_nothing_in_the_middle():
-    fp = open('test.html', 'w')
-    fp.write('{{first}}{{second}}<u>{{text}}</u>{{last}}')
-    fp.close()
+    write_file('test.html', '{{first}}{{second}}<u>{{text}}</u>{{last}}')
     try:
         t = Templater.open('test.html', marker=regexp_marker)
     except ValueError:
@@ -137,9 +142,7 @@ def test_should_not_have_named_marks_without_nothing_in_the_middle():
         assert "ValueError not raised!" == False
 
 def test_if_there_are_no_named_marker_in_the_start_of_template():
-    fp = open('test.html', 'w')
-    fp.write('<u>{{text}}</u>{{end}}')
-    fp.close()
+    write_file('test.html', '<u>{{text}}</u>{{end}}')
     try:
         t = Templater.open('test.html', marker=regexp_marker)
     except ValueError:
@@ -149,9 +152,7 @@ def test_if_there_are_no_named_marker_in_the_start_of_template():
         assert "ValueError not raised!" == False
 
 def test_raise_ValueError_if_there_is_no_named_marker_in_the_end_of_template():
-    fp = open('test.html', 'w')
-    fp.write('{{start}}<u>{{text}}</u>')
-    fp.close()
+    write_file('test.html', '{{start}}<u>{{text}}</u>')
     try:
         t = Templater.open('test.html', marker=regexp_marker)
     except ValueError:
@@ -170,10 +171,7 @@ def test_raise_ValueError_if_there_is_no_named_marker_in_the_end_of_template():
 def test_save_should_use_self_marker_if_no_marker_supplied():
     t = Templater(template='+<u>+</u>+', marker='+')
     t.save('test.html')
-    fp = open('test.html')
-    result = fp.read()
-    fp.close()
-    unlink('test.html')
+    result = read_file_and_delete('test.html')
     expected = '+<u>+</u>+'
     assert expected == result
 
@@ -181,10 +179,7 @@ def test_save_should_use_NAMED_MARKER_if_template_has_named_markers_and_no_marke
     t = Templater(template='{{one}}<u>{{two}}</u>{{three}}',
                   marker=regexp_marker)
     t.save('test.html')
-    fp = open('test.html')
-    result = fp.read()
-    fp.close()
-    unlink('test.html')
+    result = read_file_and_delete('test.html')
     named_markers = [NAMED_MARKER.format(header) for header in t._headers]
     expected = t.join(named_markers)
     assert expected == result
@@ -192,10 +187,7 @@ def test_save_should_use_NAMED_MARKER_if_template_has_named_markers_and_no_marke
 def test_save_should_use_marker_if_supplied_and_template_hasnt_named_markers():
     t = Templater(template='+<u>+</u>+', marker='+')
     t.save('test.html', marker='%%')
-    fp = open('test.html')
-    result = fp.read()
-    fp.close()
-    unlink('test.html')
+    result = read_file_and_delete('test.html')
     expected = '%%<u>%%</u>%%'
     assert expected == result
 
@@ -203,10 +195,7 @@ def test_save_should_use_python_format_if_marker_is_supplied_and_template_has_na
     t = Templater(template='{{start}}<u>{{text}}</u>{{end}}',
                   marker=regexp_marker)
     t.save('test.html', marker='[--{}--]')
-    fp = open('test.html')
-    result = fp.read()
-    fp.close()
-    unlink('test.html')
+    result = read_file_and_delete('test.html')
     expected = '[--start--]<u>[--text--]</u>[--end--]'
     assert expected == result
 
@@ -214,19 +203,13 @@ def test_save_should_use_headers_instead_of_self_headers_if_supplied():
     t = Templater(template='{{one}}<u>{{two}}</u>{{three}}',
                   marker=regexp_marker)
     t.save('test.html', headers=list('abc'))
-    fp = open('test.html')
-    result_1 = fp.read()
-    fp.close()
-    unlink('test.html')
+    result_1 = read_file_and_delete('test.html')
     named_markers = [NAMED_MARKER.format(header) for header in list('abc')]
     expected_1 = t.join(named_markers)
     assert expected_1 == result_1
 
     t.save('test.html', marker='[--{}--]', headers=list('abc'))
-    fp = open('test.html')
-    result_2 = fp.read()
-    fp.close()
-    unlink('test.html')
+    result_2 = read_file_and_delete('test.html')
     expected_2 = '[--a--]<u>[--b--]</u>[--c--]'
     assert expected_2 == result_2
 
